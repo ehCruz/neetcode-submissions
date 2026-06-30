e8995cfa759de279e5ce6e9c1dee0b395467c3bb
Eduardo Cruz <cruzeduardohenrique@gmail.com>
2026-06-30 20:54:05 -0300

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,37 @@
+class MinStack {
+
+    private Deque<Integer> minValues;
+    private Deque<Integer> stack;
+
+    public MinStack() {
+        minValues = new ArrayDeque<>();
+        stack = new ArrayDeque<>();
+    }
+
+    public void push(int val) {
+        if (minValues.isEmpty()) {
+            minValues.addFirst(val);
+        } else {
+            int min = Math.min(val, minValues.peekFirst());
+            minValues.addFirst(min);
+        }
+
+        stack.addFirst(val);
+    }
+
+    public void pop() {
+        if (stack.isEmpty()) {
+            return;
+        }
+        minValues.removeFirst();
+        stack.removeFirst();
+    }
+
+    public int top() {
+        return stack.peekFirst();
+    }
+
+    public int getMin() {
+        return minValues.peekFirst();
+    }
+}
